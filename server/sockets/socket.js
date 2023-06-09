e4ef395681ffbfa4e310d8fce8dd95232218e234
Hugo <hugo@boahstudio.com>
2023-06-09 19:35:56 -0300

    chat rooms

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -8,18 +8,20 @@ io.on('connection', (client) => {
 
     client.on('entrarChat', (data, callback) => {
     
-        if ( !data.nombre ){
+        if ( !data.nombre || !data.sala ){
             return callback({
                 error: true,
-                mensaje: 'El nombre es necesario'
+                mensaje: 'El nombre/sala es necesario'
             })
         }
 
-        let personas = usuarios.agregarPersona( client.id, data.nombre )
+        client.join(data.sala)
 
-        client.broadcast.emit('listaPersona', usuarios.getPersonas() )
+        usuarios.agregarPersona( client.id, data.nombre, data.sala )
 
-        callback(personas)
+        client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala) )
+
+        callback(usuarios.getPersonasPorSala(data.sala))
     
     })
 
@@ -27,7 +29,7 @@ io.on('connection', (client) => {
 
         let persona = usuarios.getPersona(client.id)
         let mensaje = crearMensaje( persona.nombre, data.mensaje )
-        client.broadcast.emit( 'crearMensaje', mensaje)
+        client.broadcast.to(persona.sala).emit( 'crearMensaje', mensaje)
 
     })
 
@@ -35,8 +37,8 @@ io.on('connection', (client) => {
 
         let personaBorrada = usuarios.borrarPersona( client.id )
 
-        client.broadcast.emit('crearMensaje', crearMensaje('Administrador', `${ personaBorrada.nombre } salió`))
-        client.broadcast.emit('listaPersona', usuarios.getPersonas() )
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${ personaBorrada.nombre } salió`))
+        client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala) )
     
     })
 
